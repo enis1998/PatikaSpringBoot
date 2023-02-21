6d158154113bd5192a84ed6c9819f7234b1b9d10
Enis Keskin <enis-1998@hotmail.com>
2023-02-21 14:25:09 +0300

    Initial commit

---
+++ src/main/java/com/eniskeskin/patikaspringboot/PatikaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.eniskeskin.patikaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatikaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eniskeskin/patikaspringboot/PatikaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eniskeskin.patikaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
